818cdf3f5d002ae995144c9634312bbf344bf389
Μιχαίας <mmcadoulos@gmail.com>
2024-03-01 21:54:33 -0600

    changed some styling, fixed typos, moved JS to external file

---
+++ script.js
@@ -0,0 +1,13 @@
+window.addEventListener("load", ()=>{
+    let nameChange = document.getElementById("mainHeading");
+    nameChange.addEventListener("click", ()=>{
+      if(nameChange.innerHTML == "Micah McAdoo"){
+        nameChange.innerHTML = "Μιχαίας Μάκαντου";
+      } else if (nameChange.innerHTML == "Μιχαίας Μάκαντου"){
+        nameChange.innerHTML = "מיכה" || nameChange.innerHTML == "Who am I?";
+      } else {
+        nameChange.innerHTML = "Micah McAdoo";
+      }
+      nameChange.style.color = "black";
+    });
+  });
