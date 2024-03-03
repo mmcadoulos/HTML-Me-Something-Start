37267a45d3df3f54b60dbca318e08ebdd897dbd0
Μιχαίας <mmcadoulos@gmail.com>
2024-03-02 19:45:23 -0600

    style overhaul and final draft of content

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-window.addEventListener("load", ()=>{
-    let nameChange = document.getElementById("mainHeading");
-    nameChange.addEventListener("click", ()=>{
-      if(nameChange.innerHTML == "Micah McAdoo"){
-        nameChange.innerHTML = "Μιχαίας Μάκαντου";
-      } else if (nameChange.innerHTML == "Μιχαίας Μάκαντου"){
-        nameChange.innerHTML = "מיכה" || nameChange.innerHTML == "Who am I?";
-      } else {
-        nameChange.innerHTML = "Micah McAdoo";
-      }
-      nameChange.style.color = "rgb(34, 34, 111)";
-    });
+window.addEventListener("load", () => {
+  let nameChange = document.getElementById("mainHeading");
+  nameChange.addEventListener("click", () => {
+    if (nameChange.innerHTML == "Micah McAdoo") {
+      nameChange.innerHTML = "Μιχαίας Μάκαντου";
+    } else if (nameChange.innerHTML == "Μιχαίας Μάκαντου") {
+      nameChange.innerHTML = "מיכה" || nameChange.innerHTML == "Who am I?";
+    } else {
+      nameChange.innerHTML = "Micah McAdoo";
+    }
+    nameChange.style.color = "rgb(34, 34, 111)";
   });
+});
